426e688f92f885adc1bba3215328805e5a88d2fa
Dirt Deodara <thecrowleaps@gmail.com>
2019-09-05 14:31:14 -0700

    its working

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React from "react";
+import { useAuth0 } from "../Auth0Provider";
+import Counter from "./Counter/Counter";
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const { isAuthenticated, user, loading, auth0Client } = useAuth0();
+  const login = () => auth0Client.loginWithRedirect();
+  const logout = () => auth0Client.logout();
+  console.log(isAuthenticated, "hello");
+
+  return (
+    <>
+      {!isAuthenticated && <button onClick={login}>Get up in this</button>}
+      {isAuthenticated && <button onClick={logout}>Get up out this</button>}
+      {isAuthenticated && <Counter />}
+    </>
+  );
 }
-  
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import Auth0Provider from './Auth0Provider';
 
 render(
-  <App />,
+  <Auth0Provider
+    domain="dirtdeodara.auth0.com"
+    client_id="smaRXV2o443PmGSzZP8zQWyo1aPq58m0"
+    redirect_url="http://localhost:7890"
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById('root')
 );
